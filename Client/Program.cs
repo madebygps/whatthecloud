9b5f3ca9840b7dd5251963e221ad400ac6ebc3e2
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2022-10-22 10:43:38 -0400

    Added auth package to client

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 using MudBlazor.Services;
+using Microsoft.Azure.Functions.Authentication.WebAssembly;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddStaticWebAppsAuthentication();
 
 
 builder.Services.AddMudServices();
